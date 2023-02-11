ea20a73cc368c9a03e7f36be51ade402e96332de
giovannyCaluna <36935405+giovannyCaluna@users.noreply.github.com>
2023-02-11 14:09:37 +0100

    Upload the project

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MoveButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menüToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addButtonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menüToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(756, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menüToolStripMenuItem
+            // 
+            this.menüToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addButtonToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menüToolStripMenuItem.Name = "menüToolStripMenuItem";
+            this.menüToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menüToolStripMenuItem.Text = "Menu";
+            this.menüToolStripMenuItem.Click += new System.EventHandler(this.menüToolStripMenuItem_Click);
+            // 
+            // addButtonToolStripMenuItem
+            // 
+            this.addButtonToolStripMenuItem.Name = "addButtonToolStripMenuItem";
+            this.addButtonToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addButtonToolStripMenuItem.Text = "Add button";
+            this.addButtonToolStripMenuItem.Click += new System.EventHandler(this.addButtonToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 351);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menüToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addButtonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoveButton
+{
+    public partial class Form1 : Form
+    {
+        private Point mouseLoc;
+        private int count = 0 ;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButtonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Button  btn = new Button() ;
+            btn.MouseDown += new MouseEventHandler(Mouse_LButtonDown);
+            btn.MouseMove += new MouseEventHandler(Mouse_Move);
+            btn.Name = "Button" + count.ToString() ;
+            btn.Text = "Button" + count.ToString() ;
+            ++count;
+            btn.Size = new Size(80,25);
+            btn.Location = new Point(50,50);
+            Controls.Add(btn) ;
+        }
+
+        private void Mouse_LButtonDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                mouseLoc = e.Location;
+            }
+        }
+
+        private void Mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                int x = e.X - mouseLoc.X + (sender as Button).Location.X;
+                int y = e.Y - mouseLoc.Y + (sender as Button).Location.Y;
+                (sender as Button).Location = new Point(x, y);
+            }
+        }
+
+        private void menüToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
